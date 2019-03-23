d5be4b5fa533045654ed825ab29e3f968a8f8cbb
Nimo1989 <wangxue1989320@126.com>
2019-03-23 18:22:01 +0100

    Update iets_vocabulary.py

--- iets_vocabulary.py
+++ iets_vocabulary.py
@@ -15,7 +15,7 @@ d=enchant.Dict("en_US")
 
 excludes = ['the','and','to','of','i','a','in','it','that','is',
             'you','my','with','not','his','this','but','for',
-            'me','s','he','be','as','so','him','your', 'viii']
+            'me','s','he','be','as','so','him','your', 'viii','iii','vii']
 def getText():    
     txt=open('3.txt','r').read()    
     txt=txt.lower()    
@@ -74,7 +74,7 @@ outfile2.writelines(line)
 outfile2.close()
 
 # word=('Chinese')
-for i in range(len(items)):
+for i in range(638,len(items)):
     word = items[i][0] 
     
     r = requests.get(url='http://dict.youdao.com/w/eng/%s/#keyfrom=dict2.index'%word)
@@ -85,24 +85,25 @@ for i in range(len(items)):
     #s = soup.find(class_='base-word')('ul')[0]('li')
 
     s = soup.find(class_='clearfix')('ul')[0]('li')
+    if soup.find(class_='phonetic')!=None:
     
-    s1 = soup.find(class_='phonetic').text
-    
-    
-    # pattern = re.compile(r"F\(a\((.*)\), a\((.*)\)\)")
-    pattern = re.compile(r'[\[](.*?)[\]]',re.S)
-    match = re.findall(pattern,s1)
-#    print(word)
-#    print(match)
-    with open(r'wocabulary.txt','a') as f:
-        f.write(word +"\n")
-        f.write(str(match) +"\n")
-    # 输出字典的具体内容
-    for item in s:
-        if item.text:
-            with open(r'wocabulary.txt','a') as f:              
-                f.write(item.text +"\n")
-    with open(r'wocabulary.txt','a') as f:        
-        f.write(' '+"\n")
-        f.write(' '+"\n")
-#    print('='*40+'\n')
+        s1 = soup.find(class_='phonetic').text
+        
+        
+        # pattern = re.compile(r"F\(a\((.*)\), a\((.*)\)\)")
+        pattern = re.compile(r'[\[](.*?)[\]]',re.S)
+        match = re.findall(pattern,s1)
+    #    print(word)
+    #    print(match)
+        with open(r'wocabulary.txt','a') as f:
+            f.write(word +"\n")
+            f.write(str(match) +"\n")
+        # 输出字典的具体内容
+        for item in s:
+            if item.text:
+                with open(r'wocabulary.txt','a') as f:              
+                    f.write(item.text +"\n")
+        with open(r'wocabulary.txt','a') as f:        
+            f.write(' '+"\n")
+            f.write(' '+"\n")
+    #    print('='*40+'\n')
